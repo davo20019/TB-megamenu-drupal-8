04e283d75622e74c9a62bbb040edb9a2e803b9b2
Adam Koch <adam@dogtired.org>
2017-03-08 22:48:27 -0600

    test Sass; more Gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,16 +2,29 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var tar = require('gulp-tar');
 var gzip = require('gulp-gzip');
+var clean = require('gulp-clean');
+var replace = require('gulp-replace');
+
+gulp.task('clean', function() {
+    gulp.src(['dist', 'build', 'tb_megamenu.tar.gz'], {read: false})
+        .pipe(clean());
+});
+
+gulp.task('timestamp', function() {
+    gulp.src('src/tb_megamenu.info.yml')
+        .pipe(replace('1489029834', (new Date()).getTime()))
+        .pipe(gulp.dest('./dist'));
+});
 
 gulp.task('copy', function() {
-    gulp.src('src/**/*')
-        .pipe(gulp.dest('./dist/'));
+    gulp.src(['src/**/*', '!src/**/*.sass'])
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('sass', function() {
-    gulp.src('src/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/'));
+    gulp.src('src/**/*.sass')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('tarball', function() {
@@ -21,4 +34,4 @@ gulp.task('tarball', function() {
     .pipe(gulp.dest('.'));
 });
 
-gulp.task('default', [ 'copy', 'tarball' ]);
+gulp.task('default', [ 'sass', 'copy', 'tarball' ]);
